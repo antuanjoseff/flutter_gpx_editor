59f5672e4382b969b574780d0087bba18512747f
unknown <tonidelacalle@gmail.com>
2024-12-10 11:55:41 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:gpx_editor/my_maplibre.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
 import 'package:flutter/material.dart';
 import 'package:geoxml/geoxml.dart';
-import 'package:file_picker/file_picker.dart';
 import 'dart:io';
 import 'dart:convert' show utf8;
 import 'controller.dart';
@@ -14,6 +13,11 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:double_tap_to_exit/double_tap_to_exit.dart';
 import 'util.dart';
 import 'utils/user_simple_preferences.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:file_selector/file_selector.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -78,6 +82,11 @@ class _MyHomePageState extends State<MyHomePage> {
   Color? trackColor;
   double trackWidth = 3;
 
+  final bool _isIOS = !kIsWeb && defaultTargetPlatform == TargetPlatform.iOS;
+
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
@@ -129,6 +138,70 @@ class _MyHomePageState extends State<MyHomePage> {
         });
   }
 
+  Future<void> _saveFile() async {
+    final String fileName = _nameController.text;
+    // This demonstrates using an initial directory for the prompt, which should
+    // only be done in cases where the application can likely predict where the
+    // file will be saved. In most cases, this parameter should not be provided,
+    // and in the web, path_provider shouldn't even be called.
+    final String? initialDirectory =
+        kIsWeb ? null : (await getApplicationDocumentsDirectory()).path;
+    final FileSaveLocation? result = await getSaveLocation(
+      initialDirectory: initialDirectory,
+      suggestedName: fileName,
+    );
+    if (result == null) {
+      // Operation was canceled by the user.
+      return;
+    }
+
+    final String text = _contentController.text;
+    final Uint8List fileData = Uint8List.fromList(text.codeUnits);
+    const String fileMimeType = 'text/plain';
+    final XFile textFile =
+        XFile.fromData(fileData, mimeType: fileMimeType, name: fileName);
+
+    await textFile.saveTo(result.path);
+  }
+
+  Future<(String, String)> openTextFile(BuildContext context) async {
+    const XTypeGroup typeGroup = XTypeGroup(
+      label: 'xml',
+      // extensions: <String>[],
+    );
+    // This demonstrates using an initial directory for the prompt, which should
+    // only be done in cases where the application can likely predict where the
+    // file would be. In most cases, this parameter should not be provided.
+    final String? initialDirectory =
+        kIsWeb ? null : (await getApplicationDocumentsDirectory()).path;
+    final XFile? file = await openFile(
+      // acceptedTypeGroups: <XTypeGroup>[typeGroup],
+      initialDirectory: initialDirectory,
+    );
+    if (file == null) {
+      // Operation was canceled by the user.
+      return ('', '');
+    }
+    final String fileName = file.name;
+    if (!fileName.toLowerCase().endsWith('.gpx')) {
+      return ('Incorrect format file', '');
+      // await showDialog<void>(
+      //   context: context,
+      //   builder: (BuildContext context) =>
+      //       TextDisplay('INCORRECT FORMAT', 'Chose another file'),
+      // );
+    } else {
+      final String fileContent = await file.readAsString();
+      return (fileName, fileContent);
+      // if (context.mounted) {
+      //   await showDialog<void>(
+      //     context: context,
+      //     builder: (BuildContext context) => TextDisplay(fileName, fileContent),
+      //   );
+      // }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return DoubleTapToExit(
@@ -161,18 +234,25 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   tooltip: 'Open file',
                   onPressed: () async {
-                    FilePickerResult? result =
-                        await FilePicker.platform.pickFiles();
+                    // FilePickerResult? result =
+                    //     await FilePicker.platform.pickFiles();
 
-                    if (result != null) {
-                      filename = result.files.single.path!.toString();
-                      fileName = result.files.single.name.toString();
+                    var (msg, content) = await openTextFile(context);
+                    if (content == '') {
+                      if (msg != '') {
+                        TextDisplay(msg, 'Choose a GPX file');
+                      }
+                      return;
+                    }
+                    if (content != null) {
+                      filename = msg;
+                      // fileName = result.files.single.name.toString();
 
                       try {
-                        final stream = await utf8.decoder
-                            .bind(File(filename!).openRead())
-                            .join();
-                        gpxOriginal = await GeoXml.fromGpxString(stream);
+                        // final stream = await utf8.decoder
+                        //     .bind(File(filename!).openRead())
+                        //     .join();
+                        gpxOriginal = await GeoXml.fromGpxString(content);
                         if (gpxOriginal!.trks[0].trksegs.length >= 1) {
                           showSnackBar(
                             context,
@@ -193,8 +273,6 @@ class _MyHomePageState extends State<MyHomePage> {
                       } on Exception catch (e) {
                         _dialogBuilder(context);
                       }
-                    } else {
-                      // User canceled the picker
                     }
                   },
                 ),
@@ -313,6 +391,36 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: MyMapLibre(scaffoldKey: _scaffoldKey, controller: _controller),
       ),
+    );
+  }
+}
+
+/// Widget that displays a text file in a dialog
+class TextDisplay extends StatelessWidget {
+  /// Default Constructor
+  const TextDisplay(this.fileName, this.fileContent, {super.key});
+
+  /// File's name
+  final String fileName;
+
+  /// File to display
+  final String fileContent;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(fileName),
+      content: Scrollbar(
+        child: SingleChildScrollView(
+          child: Text(fileContent),
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Close'),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ],
     );
   }
 }
